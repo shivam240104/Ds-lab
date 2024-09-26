91fe88548002903ee2a017bc7b5d8d2ffa4cea5d
shivam240104 <mauryatanmay845@gmail.com>
2024-09-26 15:59:33 +0530

    To find gcd

---
+++ Day 6/Gcd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int gcd(int a,int b){
+    if(b==0){
+        return a;
+    }
+    return gcd(b,a%b);
+}
+
+int main(){
+    int a,b;
+    printf("Enter the numbers : \n");
+    scanf("%d %d",&a,&b);
+    printf("%d",gcd(a,b));
+}
